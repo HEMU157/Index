2e4bfe6c09fc93df968294e4abeb293eb2ec6eb7
HEMU157 <97989603+HEMU157@users.noreply.github.com>
2024-01-22 08:57:35 +0530

    Add files via upload

---
+++ seo.js
@@ -0,0 +1,12 @@
+const btn=document.getElementsById("button-click");
+
+btn.addEventListner("click",function(){
+    console.log("clicked")
+})
+console.log(btn);
+
+//const btn1=document.getElementsById("button-click");
+//btn1.addEventListner("click",function(){
+//    console.log("clicked")
+//})
+//console.log(btn1);
